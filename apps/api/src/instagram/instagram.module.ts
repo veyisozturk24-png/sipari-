ff397d6d4049675e32fae48e0c358caaf4825b83
Veyis Ozturk <veyisozturk@192.168.1.107>
2026-08-02 22:51:02 +0300

    fix: load auth guards for Instagram channel

--- apps/api/src/instagram/instagram.module.ts
+++ apps/api/src/instagram/instagram.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { InstagramController } from './instagram.controller';
 import { InstagramWebhookController } from './instagram-webhook.controller';
 import { InstagramService } from './instagram.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [InstagramController, InstagramWebhookController],
   providers: [InstagramService],
 })
